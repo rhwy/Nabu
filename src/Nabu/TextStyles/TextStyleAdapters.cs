69408b7c2a71fbaa3c40cd568bfe69b0802ef1f3
rhwy <github@rui.fr>
2018-08-18 00:35:16 +0200

    rc:feat-added ansi console adapter and use vga rgb palette in html

--- src/Nabu/TextStyles/TextStyleAdapters.cs
+++ src/Nabu/TextStyles/TextStyleAdapters.cs
@@ -15,14 +15,38 @@ namespace Nabu.TextStyling
 
         static Func<string,string> getHtmlStyle = command => {
             switch(command){
-                case "red": return "color:red";
-                case "green": return "color:green";
-                case "orange": case "yellow": return "color:orange";
+                case "black": return "color:rgb(0,0,0)";
+                case "red": return "color:rgb(170,0,0)";
+                case "green": return "color:rgb(0,170,0)";
+                case "yellow": case "orange" : return "color:rgb(0,170,0)";
+                case "blue": return "color:rgb(0,0,170)";
+                case "magenta": case "pink": return "color:rgb(170,0,170)";
+                case "cyan" : return "color:rgb(0,170,170)";
+                case "white" : return "color:rgb(170,170,170)";
                 case "bold": return "font-weight:bold";
+                case "italic": return "font-style: italic;";
                 case "underline": return "text-decoration:underline";
                 default: return null;
             }
         };
+
+        static Func<string,(int start,int stop)> getConsoleCodes = command => {
+            switch(command){
+                case "black": return (30,39);
+                case "red": return (31,39);
+                case "green": return (32,39);
+                case "orange": case "yellow": return (32,39);
+                case "blue": return (34,39);
+                case "magenta": return (35,39);
+                case "cyan": return (36,39);
+                case "white": return (37,39);
+                case "bold": return (1,21);
+                case "underline": return (4,24);
+                case "italic": return (3,23);
+                default: return (-1,39);
+            }
+        };
+
 
         static Func<IEnumerable<TextCommand>,string> toHtmlText = (c) => c.Select(x=> {
             if(x.IsNormal) return x.Content;
@@ -40,9 +64,35 @@ namespace Nabu.TextStyling
                 return x.Content;
             }          
         }).Aggregate((a,b)=>a.Replace("\n","<br>")+b);
+
+        static Func<IEnumerable<TextCommand>,string> toConsoleText = (c) => c.Select(x=> {
+            if(x.IsNormal) return x.Content;
+            var styles = x.Commands
+                        .Select(m=>getConsoleCodes(m))
+                        .Where( s => s.start != -1);
+
+            if(styles.Any())
+            {
+                return "\u001b["
+                        + styles.
+                            Select(s=> s.start.ToString()).
+                            Aggregate((acc,current)=>$"{acc};{current}")
+                        + "m"
+                        + x.Content
+                        + "\u001b["
+                        + styles.
+                            Select(s=> s.stop.ToString()).
+                            Aggregate((acc,current)=>$"{acc};{current}")
+                        + "m";
+            } else {
+                return x.Content;
+            }          
+        }).Aggregate((a,b)=>a+b);
         public static Func<IEnumerable<TextCommand>,string> HtmlBasic {get;} =
         toHtmlText;
 
+        public static Func<IEnumerable<TextCommand>,string> AnsiConsole {get;} =
+        toConsoleText;
     }
 
 
--- test/Nabu.Tests/TextStyleAdaptersTests.cs
+++ test/Nabu.Tests/TextStyleAdaptersTests.cs
@@ -8,57 +8,76 @@ namespace Nabu.Tests
 {
     public class TextStyleAdaptersTests
     {
-        [Fact] public void 
-        should_get_raw_text_with_inline_raw_adapter()
+        public class ToRawText 
         {
-            var text = TextStyleParser.Print(
-                "#red`hello`",
-                c => string.Join("",c.Select(x =>x.Content)));
-            Check.That(text).IsEqualTo("hello");
-        }
-
-        [Fact] public void 
-        should_get_raw_text_with_predefined_raw_adapter()
-        {
-            var text = TextStyleParser.Print(
-                "#red`hello`", TextStyleAdapters.RawText);
-            Check.That(text).IsEqualTo("hello");
-        }
-
-        class CustomRawAdapter : ITextStyleAdapter
-        {
-            public string Adapt(IEnumerable<TextCommand> expressions)
+            [Fact] public void 
+            should_get_raw_text_with_inline_raw_adapter()
             {
-                return string.Join("",expressions.Select(x =>x.Content));
+                var text = TextStyleParser.Print(
+                    "#red`hello`",
+                    c => string.Join("",c.Select(x =>x.Content)));
+                Check.That(text).IsEqualTo("hello");
+            }
+
+            [Fact] public void 
+            should_get_raw_text_with_predefined_raw_adapter()
+            {
+                var text = TextStyleParser.Print(
+                    "#red`hello`", TextStyleAdapters.RawText);
+                Check.That(text).IsEqualTo("hello");
+            }
+
+            class CustomRawAdapter : ITextStyleAdapter
+            {
+                public string Adapt(IEnumerable<TextCommand> expressions)
+                {
+                    return string.Join("",expressions.Select(x =>x.Content));
+                }
+            }
+
+
+            [Fact] public void 
+            should_get_raw_text_with_custom_raw_adapter_interface()
+            {
+                var text = TextStyleParser.Print(
+                    "#red`hello`", new CustomRawAdapter());
+                Check.That(text).IsEqualTo("hello");
             }
         }
-        [Fact] public void 
-        should_get_raw_text_with_custom_raw_adapter_interface()
+
+        public class ToHtmlText
         {
-            var text = TextStyleParser.Print(
-                "#red`hello`", new CustomRawAdapter());
-            Check.That(text).IsEqualTo("hello");
+            [Theory] 
+            [InlineData("#red`hello`","<span style=\"color:rgb(170,0,0)\">hello</span>")] 
+            [InlineData("#red#bold`hello`","<span style=\"color:rgb(170,0,0);font-weight:bold\">hello</span>")] public void 
+            should_get_html_with_predefined_html_adapter(string source, string expectedHtml)
+            {
+                var text = TextStyleParser.Print(source, TextStyleAdapters.HtmlBasic);
+                Check.That(text).IsEqualTo(expectedHtml);
+            }
+
+            [Theory] 
+            [InlineData("#plop`hello`","hello")] 
+            [InlineData("#green#glop`hello`","<span style=\"color:rgb(0,170,0)\">hello</span>")] public void 
+            should_get_clean_html_with_predefined_html_adapter_when_style_not_known(string source, string expectedHtml)
+            {
+                var text = TextStyleParser.Print(source, TextStyleAdapters.HtmlBasic);
+                Check.That(text).IsEqualTo(expectedHtml);
+            }
         }
 
-
-        [Theory] 
-        [InlineData("#red`hello`","<span style=\"color:red\">hello</span>")] 
-        [InlineData("#red#bold`hello`","<span style=\"color:red;font-weight:bold\">hello</span>")] public void 
-        should_get_html_with_predefined_html_adapter(string source, string expectedHtml)
+        public class ToAnsiConsoleText
         {
-            var text = TextStyleParser.Print(source, TextStyleAdapters.HtmlBasic);
-            Check.That(text).IsEqualTo(expectedHtml);
-        }
+            [Theory] 
+            [InlineData("#green`hello`","\u001b[32mhello\u001b[39m")] 
+            [InlineData("#red#bold`hello`","\u001b[31;1mhello\u001b[39;21m")] public void 
+            should_get_ansi_string_with_predefined_ansi_adapter(string source, string expectedAnsiCode)
+            {
+                var text = TextStyleParser.Print(source, TextStyleAdapters.AnsiConsole);
+                Check.That(text).IsEqualTo(expectedAnsiCode);
+            }
 
-        [Theory] 
-        [InlineData("#plop`hello`","hello")] 
-        [InlineData("#green#glop`hello`","<span style=\"color:green\">hello</span>")] public void 
-        should_get_clean_html_with_predefined_html_adapter_when_style_not_known(string source, string expectedHtml)
-        {
-            var text = TextStyleParser.Print(source, TextStyleAdapters.HtmlBasic);
-            Check.That(text).IsEqualTo(expectedHtml);
         }
-         
     }
 }
 
